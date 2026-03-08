e1719439d910fddbead801923a6381491da4a128
ymy0107 <2168526598@qq.com>
2026-03-08 15:59:13 +0800

    http_proxy

---
+++ http_proxy/proxy-config.sh
@@ -0,0 +1,19 @@
+ufw reset
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 22/tcp
+ufw allow 1081/tcp
+ufw enable
+
+apt update
+apt install git make gcc build-essential -y
+wget https://github.com/3proxy/3proxy/archive/refs/tags/0.9.5.tar.gz
+tar -xvzf 0.9.5.tar.gz
+cd 3proxy-0.9.5/
+make -f Makefile.Linux -j4
+make -f Makefile.Linux install -j4
+
+cp /root/3proxy-conf/3proxy.cfg /etc/3proxy/conf/3proxy.cfg
+cp /root/3proxy-conf/passwd /etc/3proxy/conf/passwd
+systemctl restart 3proxy.service
+systemctl status 3proxy.service

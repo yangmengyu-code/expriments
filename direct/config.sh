0b30a473f66d511c051a6976183bea3894f0bf59
ymy0107 <2168526598@qq.com>
2026-03-08 17:53:56 +0800

    new

--- direct/config.sh
+++ direct/config.sh
@@ -1,3 +1,4 @@
 /root/expriments/direct/client-config.sh
 cd
 node /root/expriments/direct/client/submitd.js
+node /root/expriments/direct/client/submitd_b.js
--- http_proxy/config.sh
+++ http_proxy/config.sh
@@ -2,3 +2,4 @@ cd expriments/http_proxy
 /root/expriments/http_proxy/proxy-config.sh
 /root/expriments/http_proxy/client-config.sh
 node /root/expriments/http_proxy/client/submitp.js
+node /root/expriments/http_proxy/client/submitp_b.js

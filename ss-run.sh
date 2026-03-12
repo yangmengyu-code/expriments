a02761911322bff0e57c59fe2119613ab284f351
ymy0107 <2168526598@qq.com>
2026-03-12 13:49:51 +0800

    new

--- ss-run.sh
+++ ss-run.sh
@@ -1,8 +1,8 @@
 source /root/clashctl/scripts/cmd/clashctl.sh
 clashon
 clashtun on
-Count=$(grep -cve '^[[:space:]]*$' /root/expriments/ips.txt) - 1
-for i in $(seq 1 $((Count))); do
+Count=$(( $(grep -cve '^[[:space:]]*$' /root/expriments/ips.txt) - 1 ))
+for i in $(seq 1 $Count); do
     clashsub use $i
     printf "\n\n\n================================"
     printf "Using sub %s" "$i"

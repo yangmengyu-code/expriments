afaf2aa678c270669d2af14ec9de1213a6a7f8fb
ymy0107 <2168526598@qq.com>
2026-03-12 11:36:30 +0800

    new

---
+++ gen.sh
@@ -0,0 +1,16 @@
+find . -name "*.sh" -exec chmod +x {} +
+
+cp ./ips.txt ./http_proxy/client/ips.txt
+cp ./ips.txt ./gen-confs/clash
+cp ./ips.txt ./gen-confs/wg
+rm -rf ./gen-confs/clash/conf-*
+node ./gen-confs/clash/gen-clashconfs.js
+rm -rf ./clash/confs/*
+cp -rf ./gen-confs/clash/conf-* ./clash/confs
+#  ./gen-confs/wg/gen-keys.sh
+node ./gen-confs/wg/gen-peerinfo.js
+cp ./gen-confs/wg/peerinfo.json ./wireguard/confs/peerinfo.json
+rm -rf ./gen-confs/wg/conf-*
+node ./gen-confs/wg/gen-wgconfs.js
+rm -rf ./wireguard/confs/conf-*
+cp -rf ./gen-confs/wg/conf-* ./wireguard/confs

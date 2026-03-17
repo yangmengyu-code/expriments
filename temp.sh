f46974b42abb82fc057159b8e54b6c97c0dba717
ymy0107 <2168526598@qq.com>
2026-03-17 15:46:56 +0800

    new

--- temp.sh
+++ temp.sh
@@ -4,5 +4,28 @@ cd expriments
 git fetch --all
 git reset --hard origin/main
 find . -name "*.sh" -exec chmod +x {} +
-clashsub add /root/expriments/temp1.yaml
-clashsub add /root/expriments/temp2.yaml
+/root/expriments/shadowsocks-install.sh
+
+clear
+cd
+cd expriments
+git fetch --all
+git reset --hard origin/main
+find . -name "*.sh" -exec chmod +x {} +
+clashsub update 1
+clashsub add file:///root/expriments/temp1.yaml
+clashsub use 1
+
+clashon
+clashtun on
+clashoff
+clashtun off
+
+node /root/expriments/direct/client/submitd.js
+clear
+cd
+cd expriments
+git fetch --all
+git reset --hard origin/main
+find . -name "*.sh" -exec chmod +x {} +
+clashsub add file:///root/expriments/temp2.yaml

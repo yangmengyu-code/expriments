81320f2020a4937356a3ce595b4ef8a42c7ab20f
ymy0107 <2168526598@qq.com>
2026-03-20 16:07:14 +0800

    new

--- ss-run.sh
+++ ss-run.sh
@@ -6,7 +6,7 @@ clashtun off
 NIC="enp1s0"
 ROUND_INTERVAL=15
 # 初始时间 (所有机器需一致)
-INITIAL_TIME="2026-03-13T19:55:00+08:00"
+INITIAL_TIME="2026-03-20T16:08:00+08:00"
 COUNT=1
 # ================== 1. 加载数据 =================
 if [ ! -f "/root/expriments/ips.txt" ]; then

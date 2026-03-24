b7ea0edb704750e0da2733ab2e158cb0931d3879
ymy0107 <2168526598@qq.com>
2026-03-24 23:27:57 +0800

    Update ss-run.sh

--- ss-run.sh
+++ ss-run.sh
@@ -4,9 +4,9 @@ clashtun off
 clashoff
 # ================= 配置参数 =================
 # NIC="enp1s0"
-ROUND_INTERVAL=30
+ROUND_INTERVAL=60
 # 初始时间 (所有机器需一致)
-INITIAL_TIME="2026-03-24T23:15:00+08:00"
+INITIAL_TIME="2026-03-24T23:35:00+08:00"
 # COUNT=2
 # ================== 1. 加载数据 =================
 if [ ! -f "/root/expriments/ips.txt" ]; then

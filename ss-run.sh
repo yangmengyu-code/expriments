39837809ca03e291bcd041f308cb971b11131b6d
ymy0107 <2168526598@qq.com>
2026-03-25 17:59:31 +0800

    Update ss-run.sh

--- ss-run.sh
+++ ss-run.sh
@@ -9,7 +9,7 @@ clashoff
 ROUND_INTERVAL=50
 # 初始时间 (所有机器需一致)
 INITIAL_TIME="2026-03-25T00:06:00+08:00"
-# COUNT=2
+COUNT=3
 # ================== 1. 加载数据 =================
 if [ ! -f "/root/expriments/ips.txt" ]; then
     echo "Error: ips.txt not found"
@@ -52,50 +52,50 @@ echo "Total nodes: $N"
 LAST_START_TIME=$(date -d "$INITIAL_TIME" +%s)
 TURN=1
 
-# for (( count=1; count<=$COUNT; count++ )); do
-#     TURN=1
-while (( TURN <= N )); do
-    NOW=$(date +%s)
+for (( count=1; count<=$COUNT; count++ )); do
+    TURN=1
+    while (( TURN <= N )); do
+        NOW=$(date +%s)
 
-    if (( NOW >= LAST_START_TIME )); then
-        # echo -e "\n============= Turn $COUNT ============="
+        if (( NOW >= LAST_START_TIME )); then
+            # echo -e "\n============= Turn $COUNT ============="
 
-        if (( TURN == MY_ID )); then
-            # 接收逻辑
-            echo "RECEIVE from other peers."
-            clashtun off
-            clashoff
-            clashtun off
-            clashoff
-            sleep $(( ROUND_INTERVAL - 5 ))
-        else
-            sleep 3
-            # 发送逻辑
-            TARGET_IP=${IPS[$((TURN - 1))]}
-            echo "SEND to Peer: $TARGET_IP, Peer ID: $TURN"
-            
-            # 执行命令
-            clashtun on
-            clashon
-            clashtun on
-            clashon
-            clashsub use "$TURN"
-            clashsub use "$TURN"
-            node /root/expriments/ss_proxy/client/submitp.js
-            clashtun off
-            clashoff
-            clashtun off
-            clashoff
+            if (( TURN == MY_ID )); then
+                # 接收逻辑
+                echo "RECEIVE from other peers."
+                clashtun off
+                clashoff
+                clashtun off
+                clashoff
+                sleep $(( ROUND_INTERVAL - 5 ))
+            else
+                sleep 3
+                # 发送逻辑
+                TARGET_IP=${IPS[$((TURN - 1))]}
+                echo "SEND to Peer: $TARGET_IP, Peer ID: $TURN"
+                
+                # 执行命令
+                clashtun on
+                clashon
+                clashtun on
+                clashon
+                clashsub use "$TURN"
+                clashsub use "$TURN"
+                node /root/expriments/ss_proxy/client/submitp.js
+                clashtun off
+                clashoff
+                clashtun off
+                clashoff
+            fi
+
+            # 更新下一次开始时间戳，增加 Turn
+            LAST_START_TIME=$(( LAST_START_TIME + ROUND_INTERVAL ))
+            (( TURN++ ))
         fi
 
-        # 更新下一次开始时间戳，增加 Turn
-        LAST_START_TIME=$(( LAST_START_TIME + ROUND_INTERVAL ))
-        (( TURN++ ))
-    fi
-
-    # 每秒检查一次，防止 CPU 空转
-    sleep 1
+        # 每秒检查一次，防止 CPU 空转
+        sleep 1
+    done
 done
-
 echo "All rounds completed"
 exit 0

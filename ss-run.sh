fadd1109f37bdfb9c394890ca9bd7b07b3903487
ymy0107 <2168526598@qq.com>
2026-03-25 19:22:17 +0800

    Update ss-run.sh

--- ss-run.sh
+++ ss-run.sh
@@ -8,8 +8,8 @@ clashoff
 # NIC="enp1s0"
 ROUND_INTERVAL=50
 # 初始时间 (所有机器需一致)
-INITIAL_TIME="2026-03-25T18:12:00+08:00"
-COUNT=3
+INITIAL_TIME="2026-03-25T19:30:00+08:00"
+COUNT=2
 # ================== 1. 加载数据 =================
 if [ ! -f "/root/expriments/ips.txt" ]; then
     echo "Error: ips.txt not found"

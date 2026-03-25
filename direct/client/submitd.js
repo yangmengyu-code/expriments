90823efbfe36d959e4e53531486a96069bbbf305
ymy0107 <2168526598@qq.com>
2026-03-25 16:00:58 +0800

    Update submitd.js

--- direct/client/submitd.js
+++ direct/client/submitd.js
@@ -15,7 +15,7 @@ process.on('uncaughtException', err => {
 process.on('unhandledRejection', err => {
   console.log("IGNORED:", err.message);
 });
-const Timeout = 15000;
+const Timeout = 20000;
 const MaxCount = 10;
 async function BrowserRequest(url) {
   const browser = await chromium.launch({
